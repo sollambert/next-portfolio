4197121e25f1b3245013b1ec5a29b3c39a25b372
Solomon Lambert <sol.lambert@gmail.com>
2023-09-30 19:30:08 -0500

    hamburger menu changes

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -18,40 +18,49 @@ export default function HamburgerMenu() {
             {open ?
                 <>
                     <div className={`fixed z-10 top-0 inset-0 h-screen w-screen opacity-50 bg-stone-600 dark:bg-black`} />
-                    <div className={`px-4 py-4 fixed flex flex-col justify-between z-20 top-0 inset-0 w-min h-full bg-stone-100 dark:bg-black`}>
+                    <div className={`flex flex-col justify-between items-center px-4 py-4 fixed z-20 top-0 inset-0 w-min h-full bg-stone-100 dark:bg-black`}>
+                        <Image
+                            src="/icons/hamburger.svg"
+                            alt="Hamburger icon"
+                            className="dark:invert md:hidden"
+                            width={48}
+                            height={48}
+                            priority/>
                         <div>
-                            <HamburgerLink
-                                display="Projects"
-                                url="/projects"
-                                onClick={clickHandler} />
-                            <HamburgerLink
-                                display="Demos"
-                                url="/demos"
-                                onClick={clickHandler} />
-                            <HamburgerLink
-                                display="About"
-                                url="/about"
-                                onClick={clickHandler} />
-                        </div>
-                        <div className="mx-2 flex flex-row justify-between">
-                            <Link href="/">
+                            <div className="h-auto mb-8">
+                                <HamburgerLink
+                                    display="Projects"
+                                    url="/projects"
+                                    onClick={clickHandler} />
+                                <HamburgerLink
+                                    display="Demos"
+                                    url="/demos"
+                                    onClick={clickHandler} />
+                                <HamburgerLink
+                                    display="About"
+                                    url="/about"
+                                    onClick={clickHandler} />
+                            </div>
+                            <div className="flex flex-row justify-evenly w-full mb-8">
+                                <Link href="/">
+                                    <Image
+                                        src="/icons/home.svg"
+                                        alt="Home button"
+                                        className="dark:invert md:hidden"
+                                        width={32}
+                                        height={32}
+                                        priority
+                                        onClick={clickHandler} />
+                                </Link>
                                 <Image
-                                    src="/icons/home.svg"
-                                    alt="Home button"
+                                    src="/icons/close.svg"
+                                    alt="Close button"
                                     className="dark:invert md:hidden"
                                     width={32}
                                     height={32}
                                     priority
                                     onClick={clickHandler} />
-                            </Link>
-                            <Image
-                                src="/icons/close.svg"
-                                alt="Close button"
-                                className="dark:invert md:hidden"
-                                width={32}
-                                height={32}
-                                priority
-                                onClick={clickHandler} />
+                            </div>
                         </div>
                     </div>
                 </>
