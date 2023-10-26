77b33730a5d7a190cae04fdabccff6c19a3e7137
Solomon Lambert <sol.lambert@gmail.com>
2023-10-25 20:20:12 -0500

    add takeout container

--- app/models/[slug]/page.tsx
+++ app/models/[slug]/page.tsx
@@ -5,16 +5,21 @@ import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls } from "@react-three/drei";
 import { githubRawUrl } from "@/lib/constants";
 import { SodaCan } from '@/components/Models/Soda-Can'
+import { TakeoutContainer } from '@/components/Models/Takeout-Container'
 
 export default function ProjectPage({ params }: { params: { slug: string } }) {
 
-    const glbUrl = `${githubRawUrl}Model-Gallery/main/${params.slug}/${params.slug}.glb`
+    const glbUrl = `${githubRawUrl}Model-Gallery/main/${params.slug}.glb`
 
     const getModel = (slug: string) => {
         switch (slug) {
             case 'soda-can':
                 return (
                     <SodaCan glbUrl={glbUrl} height="100%"/>
+                )
+            case 'takeout-container':
+                return (
+                    <TakeoutContainer glbUrl={glbUrl} height="100%"/>
                 )
         }
         return <></>
@@ -26,7 +31,7 @@ export default function ProjectPage({ params }: { params: { slug: string } }) {
                 lg:w-1/2
                 flex flex-col items-start justify-evenly
                 text-blue-400 text-2xl font-bold"
-            style={{height: "50vh"}}>
+            style={{height: "75vh"}}>
             <Suspense fallback={null}>
                 {getModel(params.slug)}
                 <OrbitControls />
